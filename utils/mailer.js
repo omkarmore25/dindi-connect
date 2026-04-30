bb5b77aed6a0f366fcd01622b749281fdbaf1d85
omkarmore25 <omkarmore5178@gmail.com>
2026-05-01 01:09:21 +0530

    bug

--- utils/mailer.js
+++ utils/mailer.js
@@ -3,14 +3,14 @@ const nodemailer = require('nodemailer');
 // Create the unified transport pipeline using Gmail
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true, // Use SSL
+  port: 587,
+  secure: false, // Use STARTTLS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   },
   tls: {
-    rejectUnauthorized: false // Helps avoid handshake issues in some cloud environments
+    rejectUnauthorized: false
   }
 });
 
